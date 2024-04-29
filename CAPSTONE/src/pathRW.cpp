7e98b713f62a3338fbe37650ba5fa44ce705cf82
Chanwoochan <kcwjoma@naver.com>
2024-04-30 01:42:04 +0900

    20240430KCW_using_ros2.

---
+++ CAPSTONE/src/pathRW.cpp
@@ -0,0 +1,60 @@
+/********************************************************************
+ * @file Path_data_read_functions_for_JUJODANJO's_Manipulator
+ * @name Chan_woo Kim
+ * @version Alpha 1.0.0_
+ * @copyright Chan_woo Kim
+ *
+ * !무단 수정 금지!
+ ********************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "pathRW.hpp"
+
+void initPathData(std::ofstream &rec_path, std::string path_name)
+{
+    rec_path.open("./paths/" + path_name + ".pth");
+}
+
+void writePathData(std::ofstream &rec_path,int data_size, char *data)
+{
+    for(int i{0}; i<data_size ;i++)
+    {
+        rec_path << data[i];
+    }
+}
+
+bool openPathData(std::ifstream &path, std::string path_name)
+{
+    path.open("./paths/" + path_name + ".pth");
+    if (!path)
+	{
+		std::cerr << "Cannot open this path data." << std::endl;
+
+        return 0;
+	}
+    return 1;
+}
+
+std::string readOneLine(std::ifstream &path, int buff_size)
+{
+    char line[32];
+    path.getline(line, 32);
+    std::string line_s(line);
+    std::cout << line_s << "\n";
+    return line_s;
+}
+
+void readPathData(std::ifstream &path, int data_size, char *data)
+{
+    path.read(data,data_size);
+}
+
+void dataTransform(char *data, int *out, int motor_num)
+{
+        for(int i{0}; i<motor_num; i++)
+        {
+            out[i]=(int)((unsigned char)(data[2*i])<<8|(unsigned char)(data[2*i+1]));
+        }
+}
---
+++ CAPSTONE/src/pathRW.hpp
@@ -0,0 +1,25 @@
+/********************************************************************
+ * @file Path_data_read_functions_for_JUJODANJO's_Manipulator_HEADER
+ * @name Chan_woo Kim
+ * @version Alpha 1.0.0_
+ * @copyright Chan_woo Kim
+ *
+ * !무단 수정 금지!
+ ********************************************************************/
+
+#ifndef PATH_READ
+#define PATH_READ
+
+void initPathData(std::ofstream &rec_path, std::string path_name);
+
+void writePathData(std::ofstream &rec_path, int data_size, char *data);
+
+bool openPathData(std::ifstream &path, std::string path_name);
+
+std::string readOneLine(std::ifstream &path, int buff_size);
+
+void readPathData(std::ifstream &path, int data_size, char *data);
+
+void dataTransform(char *data, int *out, int motor_num);
+
+#endif
