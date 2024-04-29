1aea13bdd69290271f6381aeac76512238135b01
Chanwoochan <kcwjoma@kookmin.ac.kr>
2024-04-30 01:21:04 +0900

    Delete src directory

--- src/serial.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SERIAL_HPP
-#define SERIAL_HPP
-
-int openSerialPort(int *fd);
-void serialSetting(int *fd, struct termios &newtio);
-int readSerialData(int *fd, struct pollfd &poll_events, int *poll_state, char *buf, std::ofstream &rec_path);
-
-#endif
